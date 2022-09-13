3ac38895ef3c119a23cb2b032e6f9eb461ecb4eb
Niakrist <ak88dron@gmail.com>
2022-09-13 12:56:37 +0300

    feat: learning alert, prompt, cofirm

--- js/main.js
+++ js/main.js
@@ -6,12 +6,17 @@ let res = `numOne + numOne = ${numOne + numTwo}`;
 document.write(res);
 
 document.write("<br><br>");
+document.write("<b>typeof:</b>");
+document.write("<br><br>");
 
 document.write("1. undefined: ");
 document.write(typeof(undefined));
 
 document.write("<br>2. 0: ");
 document.write(typeof(0));
+
+document.write("<br>21. 10: ");
+document.write(typeof 10);
 
 document.write("<br>3. 10n: ");
 document.write(typeof(10n));
@@ -33,3 +38,25 @@ document.write(typeof(null));
 
 document.write("<br>9. alert: ");
 document.write(alert);
+
+
+document.write("<br><br>");
+document.write("<b>prompt - Введите число:</b>");
+document.write("<br><br>");
+let promptResult = prompt("Введите число", [1]);
+document.write(`Вы ввели число: ${promptResult}`);
+
+
+document.write("<br><br>");
+document.write("<b>confirm - Ты изучаешь JS?</b>");
+document.write("<br><br>");
+
+
+
+let confirmResult = confirm("Ты изучаешь JS?");
+
+if (confirmResult) {
+  document.write('Красавчик');
+} else {
+  document.write('Пора начинать');
+}
